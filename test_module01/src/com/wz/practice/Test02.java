8eef5fe353e05cf5664635bb0fa0c8f91ac29a9e
chengtufeiyang <617581984@qq.com>
2020-12-05 22:05:46 +0800

    modify  Test02.java file

--- test_module01/src/com/wz/practice/Test02.java
+++ test_module01/src/com/wz/practice/Test02.java
@@ -1,8 +1,10 @@
 package com.wz.practice;
 
 public class Test02 {
+	private string abc;
     public static void main(String[] args) {
         System.out.println("hehe");
         System.out.println("enen");
+		
     }
 }
