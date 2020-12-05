cb4d7d990c588585b07de0372ef43104a2aa44be
chengtufeiyang <617581984@qq.com>
2020-12-05 22:08:23 +0800

    增加了打印hello

--- test_module01/src/com/wz/practice/Test02.java
+++ test_module01/src/com/wz/practice/Test02.java
@@ -4,5 +4,6 @@ public class Test02 {
     public static void main(String[] args) {
         System.out.println("hehe");
         System.out.println("enen");
+        System.out.println("hello");
     }
 }
